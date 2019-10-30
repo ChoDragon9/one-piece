80b6359b78fa7d1cce70098f9dcc2e34dd11cd06
peter.cho <peter.cho@kakaocorp.com>
2019-10-30 17:36:03 +0900

    헬퍼 추가

---
+++ toggle.ts
@@ -0,0 +1,18 @@
+export class ToggleHelper {
+  private state: boolean
+  private constructor(state: boolean) {
+    this.state = state
+  }
+  on(): void {
+    this.state = true
+  }
+  off(): void {
+    this.state = false
+  }
+  get isOn(): boolean {
+    return this.state
+  }
+  static create(state: boolean): ToggleHelper {
+    return new ToggleHelper(state)
+  }
+}
