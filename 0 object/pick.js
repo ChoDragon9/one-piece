08d9e6b6f9d6102337a9e53694361f336f5ba8a1
peter.cho <peter.cho@kakaocorp.com>
2019-11-01 10:58:27 +0900

    pick 추가

--- 0 object/pick.js
+++ 0 object/pick.js
@@ -1,7 +1,11 @@
-const pick = (keys, obj) => keys
-  .map(key => ({[key]: obj[key]}))
-  .reduce((acc, obj) => Object.assign(acc, obj))
-
-const obj = {a: 1, b: 2, c: 3}
-pick(['a', 'b'], obj)
+var pick = function (keys, obj) {
+    return keys
+        .map(function (key) {
+        var _a;
+        return (_a = {}, _a[key] = obj[key], _a);
+    })
+        .reduce(function (acc, obj) { return Object.assign(acc, obj); });
+};
+var obj = { a: 1, b: 2, c: 3 };
+console.log(pick(['a', 'b'], obj));
 // {a: 1, b: 2}
