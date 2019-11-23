7cdc6090d2746f4d8d3c8056e8145bff88ba4b5d
peter.cho <peter.cho@kakaocorp.com>
2019-11-23 11:38:28 +0900

    리팩토링

--- helper-class/stepper.js
+++ helper-class/stepper.js
@@ -1,44 +1,44 @@
 "use strict";
 exports.__esModule = true;
 var Stepper = /** @class */ (function () {
-    function Stepper(min, max, step, current) {
+    function Stepper(min, max, step, position) {
         this.min = min;
         this.max = max;
         this.step = step;
-        this.current = current;
+        this.position = position;
     }
     Object.defineProperty(Stepper.prototype, "isMax", {
         get: function () {
-            return this.current === this.max;
+            return this.position === this.max;
         },
         enumerable: true,
         configurable: true
     });
     Object.defineProperty(Stepper.prototype, "isMin", {
         get: function () {
-            return this.current === this.min;
+            return this.position === this.min;
         },
         enumerable: true,
         configurable: true
     });
     Object.defineProperty(Stepper.prototype, "currentPosition", {
         get: function () {
-            return this.current;
+            return this.position;
         },
         enumerable: true,
         configurable: true
     });
     Stepper.prototype.next = function () {
-        this.changeCurrent(this.current + this.step);
+        this.changePosition(this.position + this.step);
     };
     Stepper.prototype.prev = function () {
-        this.changeCurrent(this.current - this.step);
+        this.changePosition(this.position - this.step);
     };
     Stepper.prototype.moveTo = function (position) {
-        this.changeCurrent(position);
+        this.changePosition(position);
     };
-    Stepper.prototype.changeCurrent = function (position) {
-        this.current = this.calibratePosition(position);
+    Stepper.prototype.changePosition = function (position) {
+        this.position = this.calibratePosition(position);
     };
     Stepper.prototype.calibratePosition = function (position) {
         return position > this.max
--- helper-class/stepper.ts
+++ helper-class/stepper.ts
@@ -2,37 +2,37 @@ export class Stepper {
   readonly min: number
   readonly max: number
   readonly step: number
-  private current: number
+  private position: number
 
-  private constructor(min: number, max: number, step: number, current: number) {
+  private constructor(min: number, max: number, step: number, position: number) {
     this.min = min
     this.max = max
     this.step = step
-    this.current = current
+    this.position = position
   }
 
   get isMax(): boolean {
-    return this.current === this.max
+    return this.position === this.max
   }
   get isMin(): boolean {
-    return this.current === this.min
+    return this.position === this.min
   }
   get currentPosition(): number {
-    return this.current
+    return this.position
   }
 
   next(): void {
-    this.changeCurrent(this.current + this.step)
+    this.changePosition(this.position + this.step)
   }
   prev(): void {
-    this.changeCurrent(this.current - this.step)
+    this.changePosition(this.position - this.step)
   }
   moveTo(position: number): void {
-    this.changeCurrent(position)
+    this.changePosition(position)
   }
 
-  private changeCurrent(position: number): void {
-    this.current = this.calibratePosition(position)
+  private changePosition(position: number): void {
+    this.position = this.calibratePosition(position)
   }
   private calibratePosition(position: number): number {
     return position > this.max
