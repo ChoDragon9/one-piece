95568a0a138bce7a2a0cf4eb12ad57a86228e716
peter.cho <peter.cho@kakaocorp.com>
2019-12-01 03:01:39 +0900

    리팩토링

---
+++ 0 helper/produce/persistant-merge/spec.js
@@ -0,0 +1,157 @@
+const test = (message, fn) => {
+  console.group(message)
+  fn()
+  console.groupEnd()
+}
+const assert = (src, target) => console.log(src === target)
+
+test('프로퍼티 변경 시 주소가 변경된다.', () => {
+  const baseState = {
+    done: false
+  }
+
+  const nextState = produce((draft) => {
+    draft.done = true
+  })(baseState)
+
+  assert(baseState === nextState, false)
+  assert(baseState.done === nextState.done, false)
+})
+
+
+test('변경하지 않는 객체는 변경되면 안된다.', () => {
+  const baseState = {
+    done: false,
+    inner: {
+      done: false,
+    }
+  }
+
+  const nextState = produce((draft) => {
+    draft.done = true
+  })(baseState)
+
+  assert(baseState.done === nextState.done, false)
+  assert(baseState.inner === nextState.inner, true)
+})
+
+test('객체 변경 시, 새로운 객체가 만들어진다.', () => {
+  const baseState = {
+    done: false,
+    inner: {
+      done: false,
+    }
+  }
+
+  const nextState = produce((draft) => {
+    draft.inner.done = true
+  })(baseState)
+
+  assert(baseState.done === nextState.done, true)
+  assert(baseState.inner === nextState.inner, false)
+})
+
+test('객체 변경 시, 새로운 객체가 만들어진다. - 프로퍼티 두개일 때', () => {
+  const baseState = {
+    done: false,
+    inner: {
+      txt: 'Text',
+      done: false,
+    }
+  }
+
+  const nextState = produce((draft) => {
+    draft.inner.done = true
+  })(baseState)
+
+  assert(baseState.done === nextState.done, true)
+  assert(baseState.inner === nextState.inner, false)
+  assert(baseState.inner.txt === nextState.inner.txt, true)
+})
+
+test('변경하지 않는 객체는 변경되면 안된다.', () => {
+  const baseState = {
+    done: false,
+    inner: {
+      txt: 'Text',
+      done: false,
+    },
+    inner2: {
+      txt: 'Text',
+      done: false,
+    }
+  }
+
+  const nextState = produce((draft) => {
+    draft.inner.txt = 'Hello'
+  })(baseState)
+
+  assert(baseState.inner2 === nextState.inner2, true)
+})
+
+test('변경하지 않는 객체는 변경되면 안된다. - 깊은 객체', () => {
+  const baseState = {
+    done: false,
+    inner: {
+      txt: 'Text',
+      done: false,
+    },
+    inner2: {
+      txt: 'Text',
+      done: false,
+      inner: {
+        inner: {
+          txt: 'Text',
+          done: false,
+        },
+        inner2: {
+          txt: 'Text',
+          done: false,
+        }
+      }
+    }
+  }
+
+  const nextState = produce((draft) => {
+    draft.inner2.inner.inner.txt = 'Hello'
+  })(baseState)
+
+  assert(baseState.inner === nextState.inner, true)
+  assert(baseState.inner2.inner.inner2 === nextState.inner2.inner.inner2, true)
+})
+
+test('변경이 원하는 값은 모두 정상적으로 변경되야 한다.', () => {
+  const baseState = {
+    done: false,
+    inner: {
+      txt: 'Text',
+      done: false,
+    },
+    inner2: {
+      txt: 'Text',
+      done: false,
+      inner: {
+        inner: {
+          txt: 'Text',
+          done: false,
+        },
+        inner2: {
+          txt: 'Text',
+          done: false,
+        }
+      }
+    }
+  }
+
+  const nextState = produce((draft) => {
+    draft.inner2.txt = 'Hello1'
+    draft.inner2.inner.inner.done = true
+    draft.inner2.inner.inner2.done = true
+    draft.inner2.inner.inner2.txt = 'Hello World'
+  })(baseState)
+
+  assert(nextState.inner2.txt === 'Hello1', true)
+  assert(nextState.inner2.inner.inner.done === true, true)
+  assert(nextState.inner2.inner.inner2.done === true, true)
+  assert(nextState.inner2.inner.inner2.txt === 'Hello World', true)
+})
