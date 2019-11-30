58c1e074e1012e7da0c988df48d34ff0ea9cb050
peter.cho <peter.cho@kakaocorp.com>
2019-12-01 07:06:06 +0900

    리펙토링

--- 0 helper/produce/perfomance.test.js
+++ 0 helper/produce/perfomance.test.js
@@ -1,4 +1,5 @@
 const {produce} = require('./persistant/produce')
+const {produce: produceOop} = require('./persistant-oop/produce')
 
 const clone = obj => JSON.parse(JSON.stringify(obj))
 const cloneProduce = (fn) => (state) => {
@@ -47,7 +48,8 @@ const callback = obj => {
 arr.forEach((len) => {
   console.group(len)
   run(cloneProduce(callback), len, 'cloneProduce')
-  run(produce(callback), len, 'produce')
+  run(produce(callback), len, 'produce'),
+  run(produceOop(callback), len, 'produceOop')
   console.groupEnd()
 })
 
---
+++ 0 helper/produce/persistant-oop/produce.js
@@ -0,0 +1,95 @@
+const isArray = value => Array.isArray(value)
+
+const assign = (...obj) => Object.assign(...obj)
+
+const shallowCopy = obj => {
+  if (!canProduce(obj)) return obj
+  if (isArray(obj)) return obj.concat()
+  return assign({}, obj)
+}
+
+class LinkedList {
+  constructor(base, parent = null, propName = null) {
+    this.base = base
+    this.parent = parent
+    this.propName = propName
+    this.copy = null
+  }
+  toBase() {
+    return this.copy ? this.copy : this.base
+  }
+  changeLinkedList (propName, value) {
+    const nextValue = {[propName]: value}
+
+    this.copy ?
+      assign(this.copy, nextValue) :
+      assign(this, {
+        copy: assign(shallowCopy(this.base), nextValue)
+      })
+
+    if (this.parent) {
+      this.parent.changeLinkedList(this.propName, this.copy)
+    }
+  }
+}
+
+const canProduce = value => {
+  return value === undefined || value === null ?
+    false :
+    isArray(value) || typeof value === 'object'
+}
+
+class LinkedListProxy {
+  constructor(base, parentState, propName) {
+    this.base = base
+    this.state = new LinkedList(base, parentState, propName)
+    this.children = []
+
+    const {proxy, revoke} = this.createProxy()
+    this.proxy = proxy
+    this.revokeFn = revoke
+  }
+  getter(target, propName) {
+    const value = this.state.toBase()[propName]
+    return canProduce(value) ?
+      this.createChildProxy(value, propName) :
+      value
+  }
+  setter(target, propName, value) {
+    this.state.changeLinkedList(propName, value)
+  }
+  createProxy() {
+    return Proxy.revocable(this.base, {
+      get: (...args) => this.getter(...args),
+      set: (...args) => this.setter(...args)
+    })
+  }
+  createChildProxy(value, propName) {
+    const child = LinkedListProxy.create(value, this.state, propName)
+    this.children.push(child)
+    return child.proxy
+  }
+  revoke() {
+    this.revokeFn()
+    this.children.forEach(child => child.revoke())
+  }
+  static create(base, parentState, propName) {
+    return new LinkedListProxy(base, parentState, propName)
+  }
+}
+
+const produceBase = (base, fn) => {
+  const linkedListProxy = LinkedListProxy.create(base)
+
+  fn(linkedListProxy.proxy)
+  linkedListProxy.revoke()
+
+  return linkedListProxy.state.toBase()
+}
+
+const produce = (fn) => (base) => {
+  return canProduce(base) ? produceBase(base, fn) : base
+}
+
+
+module.exports = {produce}
