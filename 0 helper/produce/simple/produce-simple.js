d535bf9acd315994fc6ed8dd83f0ed9e4a5206f6
peter.cho <peter.cho@kakaocorp.com>
2019-12-02 16:12:03 +0900

    설명 추가

--- 0 helper/produce/simple/produce-simple.js
+++ 0 helper/produce/simple/produce-simple.js
@@ -22,11 +22,11 @@ const nextState = produce(baseState, (draftState) => {
   draftState[1].done = true
 })
 
-console.log(baseState.length === 2, true)
-console.log(nextState.length === 3, true)
+console.log(baseState.length === 2) // true
+console.log(nextState.length === 3) // true
 
-console.log(baseState[1].done === false, true)
-console.log(nextState[1].done === true, true)
+console.log(baseState[1].done === false) // true
+console.log(nextState[1].done === true) // true
 
-console.log(nextState[0] === baseState[0], false)
-console.log(nextState[1] !== baseState[1], true)
+console.log(nextState[0] === baseState[0]) // false
+console.log(nextState[1] !== baseState[1]) // true
