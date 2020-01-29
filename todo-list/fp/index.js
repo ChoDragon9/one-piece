11ea4c520fe3cd1649b6f3e08a827f16cf790175
peter.cho <peter.cho@kakaocorp.com>
2020-01-29 10:35:21 +0900

    코드 추가

--- todo-list/fp/index.js
+++ todo-list/fp/index.js
@@ -3,26 +3,39 @@ class TodoList {
     this.todoList = todoList
   }
   addItem(subject, date) {
-    const todoItem = createTodoItem(subject, date)
-    return new TodoList(this.todoList.concat(todoItem))
+    const todoItem = TodoItem.create(subject, date)
+    const addedTodoList = this.todoList.concat(todoItem)
+    return TodoList.create(addedTodoList)
   }
   toString() {
     return this.todoList
-      .map(todoItemToString)
+      .map(TodoItem.todoItemToString)
       .join('\n')
+  }
+  static create(todoList) {
+    return new TodoList(todoList)
   }
 }
 
-const createTodoItem = (subject, date) => {
-  return {subject, date}
-}
-const todoItemToString = ({subject, date}) => {
-  return `[${date}] ${subject}`
+class TodoItem {
+  static create (subject, date) {
+    return {subject, date}
+  }
+  static todoItemToString ({subject, date}) {
+    return `[${date}] ${subject}`
+  }
 }
 
-const todoList = new TodoList([])
+const todoList = TodoList.create([])
   .addItem('새해인사', 20200101)
   .addItem('화분 물주기', 20200102)
+const todoList2 = todoList
   .addItem('분리수거', 20200103)
 
+console.group('todoList')
 console.log(todoList.toString())
+console.groupEnd()
+
+console.group('todoList2')
+console.log(todoList2.toString())
+console.groupEnd()
