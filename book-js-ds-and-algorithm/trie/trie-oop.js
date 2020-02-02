71ad470b152f674f705148db41039e784818cd0b
peter.cho <peter.cho@kakaocorp.com>
2020-02-03 08:56:45 +0900

    리팩토링

--- book-js-ds-and-algorithm/trie/trie-oop.js
+++ book-js-ds-and-algorithm/trie/trie-oop.js
@@ -1,25 +1,25 @@
 class TrieNode {
   constructor() {
-    this.children = {}
+    this.children = new Map()
     this.endOfWord = false
   }
   changeEnd(endOfWord) {
     this.endOfWord = endOfWord
   }
   addChild(key) {
-    this.children[key] = new TrieNode()
+    this.children.set(key, new TrieNode())
   }
   getChild(key) {
-    return this.children[key]
+    return this.children.get(key)
   }
   removeChild(key) {
-    delete this.children[key]
+    delete this.children.delete(key)
   }
   hasChild(key) {
-    return !!this.getChild(key)
+    return this.children.has(key)
   }
   hasChildren() {
-    return !!Object.keys(this.children).length
+    return this.children.size
   }
 }
 
@@ -28,13 +28,15 @@ class Trie {
     this.root = new TrieNode()
   }
   insert(word) {
-    let current = this.root
-    Array.from(word).forEach(ch => {
-      if (!current.hasChild(ch)) {
-        current.addChild(ch)
-      }
-      current = current.getChild(ch)
-    })
+    const current = Array
+      .from(word)
+      .reduce((current, ch) => {
+        if (!current.hasChild(ch)) {
+          current.addChild(ch)
+        }
+        return current.getChild(ch)
+      }, this.root)
+
     current.changeEnd(true)
   }
   search(word) {
