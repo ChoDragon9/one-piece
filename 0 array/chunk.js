48f115badf3e0c8f2e39298b95bb1c9d33fa3e43
peter <04whdydrn30@gmail.com>
2020-04-05 02:41:02 +0900

    chunk 함수 수정

--- 0 array/chunk.js
+++ 0 array/chunk.js
@@ -1,15 +1,9 @@
 const chunk = (arr, size) => {
-  if (!size) {
-    return arr
-  }
-  const length = Math.ceil(arr.length / size)
-  const newArr = Array.from({length}, () => [])
-  arr.forEach((value, index) => {
-    const newArrIndex = Math.floor(index / size)
-    newArr[newArrIndex].push(value)
-  })
-  return newArr
-}
+  if (size === 0) return arr;
+  return Array.from({length: Math.ceil(arr.length / size)}, (v, i) =>
+      arr.slice(i * size, i * size + size)
+  );
+};
 
 console.log(chunk([0, 1, 2, 3], 0)) // [0, 1, 2, 3]
 console.log(chunk([0, 1, 2, 3], 1)) // [[0], [1], [2], [3]]
