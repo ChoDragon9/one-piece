5648df5c56c1f0d160a877bb64c9b2d75538a9dd
peter.cho <peter.cho@kakaocorp.com>
2020-04-07 11:37:56 +0900

    설명 수정

--- docs/src/UIComponent/Pagination/basic/pagination.js
+++ docs/src/UIComponent/Pagination/basic/pagination.js
@@ -1,26 +1,26 @@
-let maxPage = 10
-let totalPage = 15
-let currentPage = 1
-
-let pages = []
-
-const handleNext = () => {
-  handlePage(currentPage + 1)
-  calibratePage()
-}
-
-const handlePrev = () => {
-  handlePage(currentPage - 1)
-  calibratePage()
-}
-
-const handlePage = (page) => {
-  currentPage = page
-}
-
-const calibratePage = () => {
-  const currentPageIndex = Math.ceil(currentPage / maxPage)
-  const startPage = (currentPageIndex - 1) * maxPage + 1
-  const pageLength = Math.min(maxPage, totalPage - startPage + 1)
-  pages = Array.from({length: pageLength}).map((_value, index) => startPage + index)
-}
+const props = {
+  maxPage: 10,
+  totalPage: 15,
+};
+const state = {
+  currentPage: 1,
+  get pages () {
+    const currentPageIndex = Math.ceil(state.currentPage / props.maxPage);
+    const startPage = (currentPageIndex - 1) * props.maxPage + 1;
+    const pageLength = Math.min(props.maxPage, props.totalPage - startPage + 1);
+    return Array.from({length: pageLength}).map((_value, index) => startPage + index)
+  }
+};
+const mutation = {
+  mutateCurrentPage: (page) => {
+    state.currentPage = page;
+  }
+};
+const actions = {
+  handleNext: () => {
+    mutation.mutateCurrentPage(state.currentPage + 1);
+  },
+  handlePrev: () => {
+    mutation.mutateCurrentPage(state.currentPage - 1);
+  }
+};
