6fd1a24cd8162950713cfc2ecb5c9ac52981a1ed
peter.cho <peter.cho@kakaocorp.com>
2020-05-06 00:08:29 +0900

    컴파일러 설계 추가

--- 0 study/vue-compiler/compiler.js
+++ 0 study/vue-compiler/compiler.js
@@ -4,54 +4,33 @@ const {transformer} = require('./transformer');
 const {generator} = require('./generator');
 
 const compiler = (code) => {
+  // '<h1>{{text}}</h1>'
   const tokens = lexer(code);
   // [
-  //   { type: 'word', value: 'Paper' },
-  //   { type: 'number', value: '100' }
+  //   { type: 'start', value: '<h1>' },
+  //   { type: 'template', value: '{{text}}' }
+  //   { type: 'end', value: '</h1>' }
   // ]
   const ast = parser(tokens);
   // {
-  //   body: [
-  //     {
-  //       type: 'CallExpression',
-  //       name: 'Paper',
-  //       arguments: [
-  //         {type: 'NumberLiteral', value: '100'}
-  //       ]
-  //     }
-  //   ],
-  //   type: 'Drawing'
-  // }
-  const svgAst = transformer(ast);
-  // {
-  //   tag: 'svg',
-  //   attr: {
-  //     xmlns: 'http://www.w3.org/2000/svg',
-  //     viewBox: '0 0 100 100',
-  //     version: '1.1',
-  //     width: 100,
-  //     height: 100
-  //   },
-  //   body: [
-  //     {
-  //       tag: 'rect',
-  //       attr: {
-  //         x: 0, y: 0,
-  //         width: 100, height: 100,
-  //         fill: 'rgb(0%,0%,0%)'
-  //       }
-  //     }
+  //   type: 'MarkupLanguage',
+  //   value: 'h1',
+  //   children: [
+  //     {type: 'Template', value: '{{text}}'}
   //   ]
   // }
-  const svg = generator(svgAst);
-  // <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" version="1.1" width="100" height="100">
-  //   <rect x="0" y="0" width="100" height="100" fill="rgb(0%,0%,0%)"></rect>
-  // </svg>
-  return svg;
+  const htmlAst = transformer(ast);
+  // {
+  //   tag: 'h1',
+  //   children: [
+  //     {type: 'Template', value: 'text'}
+  //   ]
+  // }
+  const renderFunction = generator(htmlAst);
+  // return createElement('h1', state.text)
+  return renderFunction;
 };
 
 const input = '<h1>{{text}}</h1>';
 const output = compiler(input);
-// (createElement, state) => {
-//   return createElement('h1', state.text)
-// }
+// return createElement('h1', state.text)
