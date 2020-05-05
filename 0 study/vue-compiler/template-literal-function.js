239ec8911610fc8bfa47f1d22b93041b881ada53
peter.cho <peter.cho@kakaocorp.com>
2020-05-06 01:55:54 +0900

    수정

--- 0 study/vue-compiler/template-literal-function.js
+++ 0 study/vue-compiler/template-literal-function.js
@@ -1,10 +1,13 @@
-const render = new Function('createElement', 'state', `return createElement('h1', state.text)`);
-// (createElement, state) => {
-//   return createElement('h1', state.text)
-// }
 const createElement = (tag, text) => `<${tag}>${text}</${tag}>`;
-const state = {
-  text: 'Message'
+const render = (state) => {
+  const renderFn = new Function(
+    'createElement',
+    'state',
+    `return createElement('h1', state.text)`
+  )
+  return renderFn(createElement, state)
 };
-console.log(render(createElement, { text: 'Message' }));
-console.log(render(createElement, { text: 'Message2' }));
+console.log(render({ text: 'Message' }));
+// <h1>Message</h1>
+console.log(render({ text: 'Message2' }));
+// <h1>Message2</h1>
