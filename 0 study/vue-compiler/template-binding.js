32321dc7c9aaf28e39f4dce643ca9f45006f9528
peter.cho <peter.cho@kakaocorp.com>
2020-05-06 01:51:33 +0900

    로깅 추가

--- 0 study/vue-compiler/template-binding.js
+++ 0 study/vue-compiler/template-binding.js
@@ -4,4 +4,6 @@ const template = '<h1>{{text}}</h1>';
 const render = compiler(template);
 
 console.log(render({ text: 'Message' }));
+// <h1>Message</h1>
 console.log(render({ text: 'Message2' }));
+// <h1>Message2</h1>
