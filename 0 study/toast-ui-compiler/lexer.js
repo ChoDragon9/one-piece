b36d7be9d174a79572162105c3e7e83627eb9a51
peter.cho <peter.cho@kakaocorp.com>
2020-05-05 19:21:35 +0900

    추가

---
+++ 0 study/toast-ui-compiler/lexer.js
@@ -0,0 +1,16 @@
+const lexer = code => {
+  return code
+    .split(/\s+/)
+    .map(token => {
+      return isNaN(token)
+        ? {type: 'word', value: token}
+        : {type: 'number', value: token};
+    });
+};
+
+const input = 'Paper 100';
+const output = lexer(input);
+// [
+//   { type: 'word', value: 'Paper' },
+//   { type: 'number', value: '100' }
+// ]
