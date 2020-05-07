bb0d75e14903681fb8ab51bb7a253c36d843bd37
peter.cho <peter.cho@kakaocorp.com>
2020-05-07 13:37:35 +0900

    추가

---
+++ 0 study/vue-compiler/v2/pseudo-code.js
@@ -0,0 +1,49 @@
+const compiler = (originCode) => {
+  const targetCode = pipe(
+    syntaxAnalyzer,
+    codeGenerator
+  )(originCode)
+  return targetCode
+}
+
+// Step 1
+const syntaxAnalyzer = (originCode) => {
+  const ast = pipe(
+    tokenizer,
+    parser
+  )(originCode)
+  return ast
+}
+
+const tokenizer = (originCode) => {
+  const tokens = []
+  return tokens
+}
+
+const parser = (tokens) => {
+  const ast = {}
+  return ast
+}
+
+// Step 2
+const codeGenerator = (ast) => {
+  const targetCode = pipe(
+    virtualCodeGenerator,
+    targetCodeGenerator,
+  )(ast)
+  return targetCode
+}
+
+const virtualCodeGenerator = (ast) => {
+  const virtualCode = {}
+  return virtualCode
+}
+
+const targetCodeGenerator = (virtualCode) => {
+  const targetCode = ''
+  return targetCode
+}
+
+const pipe = (...fns) => (arg) =>
+  fns.reduce((result, fn) => fn(result), arg)
+console.log(compiler())
