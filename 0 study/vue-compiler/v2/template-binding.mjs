edb4e8cf7750598c361389890fae012c1de8bfa2
peter.cho <peter.cho@kakaocorp.com>
2020-05-09 04:46:59 +0900

    탬플릿 바인딩 구현

--- 0 study/vue-compiler/v2/template-binding.mjs
+++ 0 study/vue-compiler/v2/template-binding.mjs
@@ -1,16 +1,17 @@
 import {compiler} from './compiler.mjs';
 
-console.time('compiler')
 const template = `<div>
   {{text}} Text
   <div>{{text}}</div>
 </div>`;
 const render = compiler(template);
 
-console.log(render({ text: 'Message' }));
-console.timeEnd('compiler')
-// <h1>Message</h1>
-console.time('compiler')
-console.log(render({ text: 'Message2' }));
-console.timeEnd('compiler')
-// <h1>Message2</h1>
+const renderTemplate = (event) => {
+  document
+    .querySelector('#template')
+    .innerHTML = render({text: event.target.value})
+}
+
+document
+  .querySelector('input')
+  .oninput = renderTemplate
