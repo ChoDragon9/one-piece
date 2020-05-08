d35eb05435194ffcf85fb52a2f456d897f153cda
peter.cho <peter.cho@kakaocorp.com>
2020-05-08 16:25:28 +0900

    문법 및 플랜추가

--- 0 study/vue-compiler/v2/compiler.js
+++ 0 study/vue-compiler/v2/compiler.js
@@ -3,86 +3,57 @@ import {parser} from './parser.js';
 import {transformer} from './transformer.js';
 import {generator} from './generator.js';
 
-// Formal Grammar
-// S ::= <Tag>
-// <Tag> ::= <Start> <End>
-//   | <Text>
-//   | <Start> <Tag> <End>
-// <Start> ::= `<`<Text>`>`
-// <End> ::= `</`<Text>`>`
-// <Text> ::= string | number
+/*
+Grammar
+Keyword: 'div' | 'span'
+Symbol: '<' | '>' | '</' | '{' | '}'
+StringConstant: 문자열
+
+Tag: '<' keyword '>' (Tag | Template | StringConstant) '</' keyword '>'
+Template: '{{' stringConstant '}}'
+*/
 
 export const compiler = (code) => {
+  // Step 1.
+  // `<div>Text</div>`
+  // Step 2.
+  // `<div>{{text}} Text</div>`
+  // Step 3.
+  // `<div>{{text}} Text<div>{{text}}</div></div>`
+  // Step 4.
   // `<div>
   //   {{text}} Text
   //   <div>{{text}}</div>
   // </div>`
   const tokens = lexer(code);
-  // [
-  //   { type: 'Start', value: '<div>' },
-  //   { type: 'Text', value: '{{text}} Text' },
-  //   { type: 'Start', value: '<div>' },
-  //   { type: 'Text', value: '{{text}}' },
-  //   { type: 'End', value: '</div>' },
-  //   { type: 'End', value: '</div>' }
-  // ]
+  // Step 1.
+  // ['<', 'div', '>', 'Text', '</', 'div', '>']
   const ast = parser(tokens);
+  // Step 1.
   // {
-  //   type: 'MarkupLanguage',
+  //   type: 'Tag',
   //   body: [
-  //     { type: 'StartTag', value: '<div>' },
-  //     {
-  //       type: 'Content',
-  //       body: [
-  //         { type: 'Text', value: '{{text}} Text' },
-  //         { type: 'StartTag', value: '<div>' },
-  //         {
-  //           type: 'Content',
-  //           body: [
-  //             { type: 'Text', value: '{{text}}' },
-  //           ]
-  //         },
-  //         { type: 'EndTag', value: '</div>' },
-  //       ]
-  //     },
-  //     { type: 'EndTag', value: '</div>' }
+  //     { type: 'Symbol', value: '<' },
+  //     { type: 'Keyword', value: 'div' },
+  //     { type: 'Symbol', value: '>' },
+  //     { type: 'Symbol', value: '</' },
+  //     { type: 'StringConstant', value: 'Text' },
+  //     { type: 'Symbol', value: '</' },
+  //     { type: 'Keyword', value: 'div' },
+  //     { type: 'Symbol', value: '>' },
   //   ]
   // }
   const htmlAst = transformer(ast);
-  // {
-  //   type: 'Element',
-  //   children: [
-  //     {type: 'StartTag', value: '<div>'},
-  //     {
-  //       type: 'Element',
-  //       children: [
-  //         {type: 'Template', value: '{{text}}'},
-  //         {type: 'Text', value: ' Text'},
-  //         {type: 'StartTag', value: '<div>'},
-  //         {
-  //           type: 'Element',
-  //           children: [
-  //             {type: 'Template', value: '{{text}}'},
-  //           ]
-  //         },
-  //         {type: 'EndTag', value: '</div>'}
-  //       ]
-  //     },
-  //     {type: 'EndTag', value: '</div>'}
-  //   ]
-  // }
+  // Step 1.
+  // createElement([
+  //   createStartElement('div'),
+  //   createText('Text')
+  //   createEndElement('div'),
+  // ])
   const renderFn = generator(htmlAst);
   // (state) => createElement([
   //   createStartElement('div'),
-  //   createElement([
-  //     createTemplate(state.text),
-  //     createText(' Text'),
-  //     createElement([
-  //       createStartElement('div'),
-  //       createTemplate(state.text),
-  //       createEndElement('div'),
-  //     ]),
-  //   ]),
+  //   createText('Text')
   //   createEndElement('div'),
   // ])
   return renderFn;
