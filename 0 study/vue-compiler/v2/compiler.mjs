b2f894614547f1c6ee85b5ad2a69edb3b1b30325
peter.cho <peter.cho@kakaocorp.com>
2020-05-09 13:36:15 +0900

    리팩토링

--- 0 study/vue-compiler/v2/compiler.mjs
+++ 0 study/vue-compiler/v2/compiler.mjs
@@ -1,16 +1,6 @@
 import {syntaxAnalyzer} from './syntax-analyzer/index.mjs';
 import {codeGenerator} from './code-generator/index.mjs';
 
-/*
-Grammar
-Keyword: 'div' | 'span'
-Symbol: '<' | '>' | '</' | '{' | '}'
-StringConstant: 문자열
-
-Tag: '<' keyword '>' (Tag | Template | StringConstant) '</' keyword '>'
-Template: '{{' stringConstant '}}'
-*/
-
 export const compiler = (originCode) => {
   const ast = syntaxAnalyzer(originCode);
   const renderFn = codeGenerator(ast);
